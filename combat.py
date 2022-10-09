f7a9536a7fc686516038916198d4c6fb7ae596ae
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2022-10-09 17:30:40 -0400

    Combat v2
    Fighter stat inputs given as dict now
    Fighter has a name variable, displayed by battle function
    Round counter added to battle (for visibility, may remove later)

--- combat.py
+++ combat.py
@@ -1,37 +1,61 @@
+monster_test = {
+    "name": "Goofy Goblin",
+    "hp": 10,
+    "atk": 2
+}
+player_test = {
+    "name": "Big Hero Man",
+    "hp": 100,
+    "atk": 1
+}
+
+
 class Fighter():
-    def __init__(self, hp_in, atk_in):
-        self.atk = atk_in
-        self.hp = hp_in
+    def __init__(self, stats):
+        self.name = stats.get("name")
+        self.atk = stats.get("atk")
+        self.hp = stats.get("hp")
     def damage(self, dmg):
         self.hp -= dmg
     def attack(self):
         return self.atk
     def is_alive(self):
         return self.hp > 0
-    def current_hp(self):
+    def get_hp(self):
         return self.hp
+    def get_name(self):
+        return self.name
 
-def battle(f1, f2):
+def battle(f1: Fighter, f2: Fighter):
+    f1_name = f1.get_name()
+    f2_name = f2.get_name()
+    round = 1
+    
     while f1.is_alive() and f2.is_alive():
+        
+        print(f"\n ===== Round {round} ===== ")
+
         f1_attack = f1.attack()
         f2.damage(f1_attack)
-        print(f"Fighter 1 hits fighter 2 for {f1_attack} damage, leaving fighter 2 with {f2.current_hp()} health")
+        print(f"{f1_name} hits {f2_name} for {f1_attack} damage, leaving {f2_name} with {f2.get_hp()} health")
         f2_attack = f2.attack()
         f1.damage(f2_attack)
-        print(f"Fighter 2 hits fighter 1 for {f2_attack} damage, leaving fighter 1 with {f1.current_hp()} health")
+        print(f"{f2_name} hits {f1_name} for {f2_attack} damage, leaving {f1_name} with {f1.get_hp()} health")
+        
+        round += 1
     
     if f1.is_alive():
-        print("Fighter 2 is slain. Fighter 1 is victorious.")
+        print(f"{f2_name} is slain. {f1_name} is victorious.")
         return f1
     elif f2.is_alive():
-        print("Fighter 1 is slain. Fighter 2 is victorious.")
+        print(f"{f1_name} is slain. {f2_name} is victorious.")
         return f2
     else:
         print("Both fighters slay each other simultaneously")
         return None
 
 def test_battle():
-    f1 = Fighter(10,2)
-    f2 = Fighter(4,1)
-    battle(f1, f2)
+    monster = Fighter(monster_test)
+    player = Fighter(player_test)
+    battle(player, monster)
 test_battle()
