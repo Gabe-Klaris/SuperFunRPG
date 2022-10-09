e2eec1ed2186944836516a51a37ba8c1a2be1558
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2022-10-09 18:12:42 -0400

    Combat v3
    Overhauled attack system. Attack class that holds info about attack, evaluated by fighter class. Defense statistic added which reduces damage taken (can be ignored by some attacks).

--- combat.py
+++ combat.py
@@ -1,24 +1,33 @@
-monster_test = {
-    "name": "Goofy Goblin",
-    "hp": 10,
-    "atk": 2
-}
-player_test = {
-    "name": "Big Hero Man",
-    "hp": 100,
-    "atk": 1
-}
+class Attack():
+    def __init__(self, base, ignores_dfn = False):
+        self.base = base
+        self.ignores_dfn = ignores_dfn
+    def execute(self):
+        return {"quantity": self.base, "ignores_dfn": self.ignores_dfn}
 
 
 class Fighter():
-    def __init__(self, stats):
+    def __init__(self, stats, attacks = None):
         self.name = stats.get("name")
-        self.atk = stats.get("atk")
+        self.str = stats.get("str")
         self.hp = stats.get("hp")
-    def damage(self, dmg):
-        self.hp -= dmg
+        self.dfn = stats.get("dfn")
+        if attacks:
+            self.attacks = attacks
+        else:
+            self.attacks = stats.get("attack")
+
+    def damage(self, damage):
+        taken = damage.get("quantity")
+        if not damage.get("ignores_dfn"):
+            taken *= 1/(2**(self.dfn/100))
+        self.hp -= taken
+
     def attack(self):
-        return self.atk
+        attack_out = self.attacks.execute()
+        attack_out["quantity"] *= self.str
+        return attack_out
+
     def is_alive(self):
         return self.hp > 0
     def get_hp(self):
@@ -26,21 +35,38 @@ class Fighter():
     def get_name(self):
         return self.name
 
+
+slash = Attack(base = 5)
+stab = Attack(base = 2, ignores_dfn = True)
+monster_test = {
+    "name": "Goofy Goblin",
+    "hp": 10,
+    "str": 2,
+    "dfn": 0,
+    "attack": stab
+}
+player_test = {
+    "name": "Big Hero Man",
+    "hp": 100,
+    "str": 1,
+    "dfn": 50
+}
+
 def battle(f1: Fighter, f2: Fighter):
     f1_name = f1.get_name()
     f2_name = f2.get_name()
     round = 1
-    
+
     while f1.is_alive() and f2.is_alive():
         
         print(f"\n ===== Round {round} ===== ")
 
         f1_attack = f1.attack()
         f2.damage(f1_attack)
-        print(f"{f1_name} hits {f2_name} for {f1_attack} damage, leaving {f2_name} with {f2.get_hp()} health")
+        print(f"{f1_name} hits {f2_name} for {f1_attack['quantity']} damage, leaving {f2_name} with {f2.get_hp():0.1f} health")
         f2_attack = f2.attack()
         f1.damage(f2_attack)
-        print(f"{f2_name} hits {f1_name} for {f2_attack} damage, leaving {f1_name} with {f1.get_hp()} health")
+        print(f"{f2_name} hits {f1_name} for {f2_attack['quantity']} damage, leaving {f1_name} with {f1.get_hp():0.1f} health")
         
         round += 1
     
@@ -56,6 +82,6 @@ def battle(f1: Fighter, f2: Fighter):
 
 def test_battle():
     monster = Fighter(monster_test)
-    player = Fighter(player_test)
+    player = Fighter(player_test, attacks = slash)
     battle(player, monster)
 test_battle()
