9033703c828b6ff888c5016d77642b68ea3f54b4
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2022-10-11 22:38:08 -0400

    Combat v5
    Player class extends fighter to give some player control. Revamped combat display.

--- combat.py
+++ combat.py
@@ -1,10 +1,13 @@
 import random
+import os
 
 class Attack():
     def __init__(self, name = "an attack", base = 1, ignores_dfn = False):
         self.name = name
         self.base = base
         self.ignores_dfn = ignores_dfn
+    def get_name(self):
+        return self.name
     def execute(self, str):
         return {"name": self.name, "quantity": self.base*str, "ignores_dfn": self.ignores_dfn}
 
@@ -38,6 +41,14 @@ class Fighter():
     def get_name(self):
         return self.name
 
+class Player(Fighter):
+    def attack(self):
+        for num, attack in enumerate(self.attacks):
+            print(f"{num+1}: {attack.get_name()}")
+        attack_num = int(input("Which attack: "))
+        this_attack = self.attacks[attack_num-1]
+        attack_out = this_attack.execute(self.str)
+        return attack_out
 
 slash = Attack("slash", base = 5)
 stab = Attack("stab", base = 2, ignores_dfn = True)
@@ -57,22 +68,41 @@ player_test = {
     "dfn": 50
 }
 
+def display_battle(f1, f2):
+    size = os.get_terminal_size()
+    width = size[0]
+    height = size[1]
+    print()
+    print(f"{f1.get_name()} versus {f2.get_name()}".center(width))
+    print("\n" * (height//2-3), end="")
+    print("".ljust(width//10)
+        + str(f1.get_name()).ljust(width//10*4) 
+        + str(f2.get_name()).rjust(width//10*4)
+        )
+    print("".ljust(width//10)
+        + "HP: " f"{f1.get_hp():0.1f}".ljust(width//10*4) 
+        + "HP: " f"{f2.get_hp():0.1f}".rjust(width//10*4)
+        )
+    print("\n"*(height//2-3), end="")
+    print()
+
 def battle(f1: Fighter, f2: Fighter):
     f1_name = f1.get_name()
     f2_name = f2.get_name()
-    round = 1
 
     while f1.is_alive() and f2.is_alive():
-        
-        print(f"\n ===== Round {round} ===== ")
 
+        display_battle(f1, f2)
         f1_attack = f1.attack()
         f2.damage(f1_attack)
         print(f"{f1_name} uses {f1_attack['name']} on {f2_name} for {f1_attack['quantity']} damage, leaving {f2_name} with {f2.get_hp():0.1f} health")
+        input("[Enter to continue]")
+
+        display_battle(f1, f2)
         f2_attack = f2.attack()
         f1.damage(f2_attack)
         print(f"{f2_name} uses {f2_attack['name']} on {f1_name} for {f2_attack['quantity']} damage, leaving {f1_name} with {f1.get_hp():0.1f} health")
-        round += 1
+        input("[Enter to continue]")
     
     if f1.is_alive():
         print(f"{f2_name} is slain. {f1_name} is victorious.")
@@ -84,8 +114,10 @@ def battle(f1: Fighter, f2: Fighter):
         print("Both fighters slay each other simultaneously")
         return None
 
+
+
 def test_battle():
     monster = Fighter(monster_test)
-    player = Fighter(player_test, attacks = [bash, slam])
+    player = Player(player_test, attacks = [bash, slam])
     battle(player, monster)
 test_battle()
