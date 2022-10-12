7f5a5e22eaf12bb0096f5d74e1171fb897ecf421
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2022-10-11 23:00:45 -0400

    Combat v6
    Added speed stat to fighters and attacks, changing the way that turn order is determined.

--- combat.py
+++ combat.py
@@ -2,12 +2,15 @@ import random
 import os
 
 class Attack():
-    def __init__(self, name = "an attack", base = 1, ignores_dfn = False):
+    def __init__(self, name = "an attack", base = 1, speed = 1, ignores_dfn = False):
         self.name = name
         self.base = base
+        self.speed = speed
         self.ignores_dfn = ignores_dfn
     def get_name(self):
         return self.name
+    def get_speed(self):
+        return self.speed
     def execute(self, str):
         return {"name": self.name, "quantity": self.base*str, "ignores_dfn": self.ignores_dfn}
 
@@ -18,6 +21,7 @@ class Fighter():
         self.str = stats.get("str")
         self.hp = stats.get("hp")
         self.dfn = stats.get("dfn")
+        self.speed = stats.get("speed")
         if attacks:
             self.attacks = attacks
         else:
@@ -32,7 +36,9 @@ class Fighter():
     def attack(self):
         this_attack = random.choice(self.attacks)
         attack_out = this_attack.execute(self.str)
+        self.current_speed -= this_attack.get_speed()
         return attack_out
+
 
     def is_alive(self):
         return self.hp > 0
@@ -40,32 +46,39 @@ class Fighter():
         return self.hp
     def get_name(self):
         return self.name
+    def reset_turn(self):
+        self.current_speed = self.speed
+    def is_turn(self):
+        return self.current_speed > 0
 
 class Player(Fighter):
     def attack(self):
         for num, attack in enumerate(self.attacks):
-            print(f"{num+1}: {attack.get_name()}")
+            print(f"{num+1}: {attack.get_name()} ({attack.get_speed()} speed)")
         attack_num = int(input("Which attack: "))
         this_attack = self.attacks[attack_num-1]
         attack_out = this_attack.execute(self.str)
+        self.current_speed -= this_attack.get_speed()
         return attack_out
 
-slash = Attack("slash", base = 5)
-stab = Attack("stab", base = 2, ignores_dfn = True)
-bash = Attack("bash", base = 10)
-slam = Attack("slam", 5, True)
+slash = Attack("slash", base = 5, speed = 3)
+stab = Attack("stab", base = 1, ignores_dfn = True, speed = 1)
+bash = Attack("bash", base = 10, speed = 4)
+slam = Attack("slam", base = 5, ignores_dfn = True, speed = 2)
 monster_test = {
     "name": "Goofy Goblin",
-    "hp": 10,
+    "hp": 50,
     "str": 2,
     "dfn": 0,
-    "attacks": [stab, slash]
+    "attacks": [stab, slash],
+    "speed": 10
 }
 player_test = {
     "name": "Big Hero Man",
     "hp": 100,
     "str": 1,
-    "dfn": 50
+    "dfn": 50,
+    "speed": 5
 }
 
 def display_battle(f1, f2):
@@ -91,18 +104,21 @@ def battle(f1: Fighter, f2: Fighter):
     f2_name = f2.get_name()
 
     while f1.is_alive() and f2.is_alive():
+        f1.reset_turn()
+        while f1.is_turn():
+            display_battle(f1, f2)
+            f1_attack = f1.attack()
+            f2.damage(f1_attack)
+            print(f"{f1_name} uses {f1_attack['name']} on {f2_name} for {f1_attack['quantity']} damage, leaving {f2_name} with {f2.get_hp():0.1f} health")
+            input("[Enter to continue]")
 
-        display_battle(f1, f2)
-        f1_attack = f1.attack()
-        f2.damage(f1_attack)
-        print(f"{f1_name} uses {f1_attack['name']} on {f2_name} for {f1_attack['quantity']} damage, leaving {f2_name} with {f2.get_hp():0.1f} health")
-        input("[Enter to continue]")
-
-        display_battle(f1, f2)
-        f2_attack = f2.attack()
-        f1.damage(f2_attack)
-        print(f"{f2_name} uses {f2_attack['name']} on {f1_name} for {f2_attack['quantity']} damage, leaving {f1_name} with {f1.get_hp():0.1f} health")
-        input("[Enter to continue]")
+        f2.reset_turn()
+        while f2.is_turn():
+            display_battle(f1, f2)
+            f2_attack = f2.attack()
+            f1.damage(f2_attack)
+            print(f"{f2_name} uses {f2_attack['name']} on {f1_name} for {f2_attack['quantity']} damage, leaving {f1_name} with {f1.get_hp():0.1f} health")
+            input("[Enter to continue]")
     
     if f1.is_alive():
         print(f"{f2_name} is slain. {f1_name} is victorious.")
