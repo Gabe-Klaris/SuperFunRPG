f8e35051d1e0992cc43ebd867a0e1e6fb24d930c
Henry-Cat <57220048+Henry-Cat@users.noreply.github.com>
2022-10-11 23:10:28 -0400

    Combat 0.1.0
    Made various internal fixes.
    Battle ends at right time now.
    Display changes.
    
    A now complete and viable, if primitive, combat system, hence the change in naming scheme

--- combat.py
+++ combat.py
@@ -2,15 +2,15 @@ import random
 import os
 
 class Attack():
-    def __init__(self, name = "an attack", base = 1, speed = 1, ignores_dfn = False):
+    def __init__(self, name = "an attack", base = 1, time = 1, ignores_dfn = False):
         self.name = name
         self.base = base
-        self.speed = speed
+        self.time = time
         self.ignores_dfn = ignores_dfn
     def get_name(self):
         return self.name
-    def get_speed(self):
-        return self.speed
+    def get_time(self):
+        return self.time
     def execute(self, str):
         return {"name": self.name, "quantity": self.base*str, "ignores_dfn": self.ignores_dfn}
 
@@ -21,7 +21,7 @@ class Fighter():
         self.str = stats.get("str")
         self.hp = stats.get("hp")
         self.dfn = stats.get("dfn")
-        self.speed = stats.get("speed")
+        self.spd = stats.get("spd")
         if attacks:
             self.attacks = attacks
         else:
@@ -36,7 +36,7 @@ class Fighter():
     def attack(self):
         this_attack = random.choice(self.attacks)
         attack_out = this_attack.execute(self.str)
-        self.current_speed -= this_attack.get_speed()
+        self.time -= this_attack.get_time()
         return attack_out
 
 
@@ -47,47 +47,48 @@ class Fighter():
     def get_name(self):
         return self.name
     def reset_turn(self):
-        self.current_speed = self.speed
+        self.time = self.spd
     def is_turn(self):
-        return self.current_speed > 0
+        return self.time > 0
 
 class Player(Fighter):
     def attack(self):
+        print(f"Select an attack ({self.time} time remaining)")
         for num, attack in enumerate(self.attacks):
-            print(f"{num+1}: {attack.get_name()} ({attack.get_speed()} speed)")
-        attack_num = int(input("Which attack: "))
+            print(f"{num+1}: {attack.get_name()} ({attack.get_time()} time)")
+        attack_num = int(input())
         this_attack = self.attacks[attack_num-1]
         attack_out = this_attack.execute(self.str)
-        self.current_speed -= this_attack.get_speed()
+        self.time -= this_attack.get_time()
         return attack_out
 
-slash = Attack("slash", base = 5, speed = 3)
-stab = Attack("stab", base = 1, ignores_dfn = True, speed = 1)
-bash = Attack("bash", base = 10, speed = 4)
-slam = Attack("slam", base = 5, ignores_dfn = True, speed = 2)
+slash = Attack("slash", base = 5, time = 3)
+stab = Attack("stab", base = 1, ignores_dfn = True, time = 1)
+bash = Attack("bash", base = 10, time = 4)
+slam = Attack("slam", base = 5, ignores_dfn = True, time = 2)
 monster_test = {
     "name": "Goofy Goblin",
     "hp": 50,
     "str": 2,
     "dfn": 0,
     "attacks": [stab, slash],
-    "speed": 10
+    "spd": 10
 }
 player_test = {
     "name": "Big Hero Man",
     "hp": 100,
     "str": 1,
     "dfn": 50,
-    "speed": 5
+    "spd": 5
 }
 
 def display_battle(f1, f2):
     size = os.get_terminal_size()
     width = size[0]
     height = size[1]
-    print()
+    print("\n"*5)
     print(f"{f1.get_name()} versus {f2.get_name()}".center(width))
-    print("\n" * (height//2-3), end="")
+    print("\n" * (height//2-4), end="")
     print("".ljust(width//10)
         + str(f1.get_name()).ljust(width//10*4) 
         + str(f2.get_name()).rjust(width//10*4)
@@ -96,14 +97,14 @@ def display_battle(f1, f2):
         + "HP: " f"{f1.get_hp():0.1f}".ljust(width//10*4) 
         + "HP: " f"{f2.get_hp():0.1f}".rjust(width//10*4)
         )
-    print("\n"*(height//2-3), end="")
+    print("\n"*(height//2-4), end="")
     print()
 
 def battle(f1: Fighter, f2: Fighter):
     f1_name = f1.get_name()
     f2_name = f2.get_name()
 
-    while f1.is_alive() and f2.is_alive():
+    while True:
         f1.reset_turn()
         while f1.is_turn():
             display_battle(f1, f2)
@@ -112,6 +113,10 @@ def battle(f1: Fighter, f2: Fighter):
             print(f"{f1_name} uses {f1_attack['name']} on {f2_name} for {f1_attack['quantity']} damage, leaving {f2_name} with {f2.get_hp():0.1f} health")
             input("[Enter to continue]")
 
+            if not f2.is_alive():
+                print(f"{f2_name} is slain. {f1_name} is victorious.")
+                return f1
+
         f2.reset_turn()
         while f2.is_turn():
             display_battle(f1, f2)
@@ -119,16 +124,10 @@ def battle(f1: Fighter, f2: Fighter):
             f1.damage(f2_attack)
             print(f"{f2_name} uses {f2_attack['name']} on {f1_name} for {f2_attack['quantity']} damage, leaving {f1_name} with {f1.get_hp():0.1f} health")
             input("[Enter to continue]")
-    
-    if f1.is_alive():
-        print(f"{f2_name} is slain. {f1_name} is victorious.")
-        return f1
-    elif f2.is_alive():
-        print(f"{f1_name} is slain. {f2_name} is victorious.")
-        return f2
-    else:
-        print("Both fighters slay each other simultaneously")
-        return None
+
+            if not f1.is_alive():
+                print(f"{f1_name} is slain. {f2_name} is victorious.")
+                return f2
 
 
 
